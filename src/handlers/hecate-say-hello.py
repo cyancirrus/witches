b664d05d8bd27fd978754c1f4d7844a245892e0f
Raven Echo <ravenecho@Ravens-MacBook-Pro.local>
2021-03-09 11:03:02 -0600

    removing some redundant? permissions

--- src/handlers/hecate-say-hello.py
+++ src/handlers/hecate-say-hello.py
@@ -4,5 +4,5 @@ import numpy
 def hecateFunction(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello my name is Hecate, from Lambda!' + str(numpy.array((1,2,3))))
+        'body': json.dumps('Hello my name is Hecate, from Lambda whats up!' + str(numpy.array((1,2,3))))
     }
