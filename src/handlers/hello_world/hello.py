28964b990c974007208892adca27da2347880ed1
Raven Echo <ravenecho@Ravens-MacBook-Pro.local>
2021-03-09 13:19:41 -0600

    hmm

---
+++ src/handlers/hello_world/hello.py
@@ -0,0 +1,9 @@
+import json
+# this path is valid from the api instance
+from src.handlers.hello_world.world import myString
+
+def helloFunction(event, context):
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello my name is HELLO, from Lambda whats up!' + myString())
+    }
