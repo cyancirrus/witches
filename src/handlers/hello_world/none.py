e2141a6facc46921ed604905b33b03789233930f
Raven Echo <ravenecho@Ravens-MacBook-Pro.local>
2021-03-10 14:59:17 -0600

    what?

--- src/handlers/hello_world/none.py
+++ src/handlers/hello_world/none.py
@@ -2,7 +2,7 @@ import json
 import requests
 
 url = "http://127.0.0.1:3000/goddess/helloworld"
-
+url = " https://mikqk5fn4m.execute-api.us-east-1.amazonaws.com/Prod/goddess/helloworld"
  
 r = requests.get(url,
     headers = {
