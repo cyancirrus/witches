e8f08566b92d09b3dc8b2eeb2c6d279ed7fd9873
Raven Echo <ravenecho@Ravens-MacBook-Pro.local>
2021-03-10 14:18:30 -0600

    please work

--- src/handlers/hello_world/hello.py
+++ src/handlers/hello_world/hello.py
@@ -3,7 +3,10 @@ import json
 from src.handlers.hello_world.world import myString
 
 def helloFunction(event, context):
+    eventClean = json.loads(event['body'])
+    name = dict(eventClean)["hello"]
+
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello my name is HELLO, from Lambda whats up!' + myString() + str(event['body']['hello']))
+        'body': json.dumps('Hello my name is HELLO, from Lambda whats up!' + myString() + name)
     }
---
+++ src/handlers/hello_world/none.py
@@ -0,0 +1,17 @@
+import json
+import requests
+
+url = "http://127.0.0.1:3000/goddess/helloworld"
+
+ 
+r = requests.get(url,
+    headers = {
+        'Accept' : 'text/json'
+    },
+    json = {
+        "hello":"is this still good"
+    }
+)
+
+print(r.text)
+print(r.status_code)
